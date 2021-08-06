d69b791ceacb0ac37d6093791cbfa3d3e9e21ffa
beharavenkatasatyaprasad <satyaprasadbehara@gmail.com>
2021-08-06 22:12:17 +0530

    initiated backend repo

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express"),
+  app = express(),
+  mongoose = require("mongoose"),
+  cors = require("cors"),
+  morgan = require("morgan"),
+  parser = require("body-parser");
+
+  
